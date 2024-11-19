42ad99572e99591731c8bb903130ad0514b6cd7a
CHANDAN KUMAR SAHNI <142076813+chandan1504@users.noreply.github.com>
2024-11-19 04:45:41 +0000

    conversion done

---
+++ 01-basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+// let score  = "15"
+// let score  = "15abc"
+// let score  = null
+// let score  = undefined
+let score  = true
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//" 15" => 15
+// "15abc => NaN"
+// true => 1; false => 0
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "Chandan"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+ //-----------------/*Conversion*?--------------------
+// 1 => true; 0 => false
+// "" => false
+//"Chandan"  => true
+
+
+ let someNumber  = 30
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log( typeof stringNumber)
