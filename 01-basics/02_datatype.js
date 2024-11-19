53c743d58b79c8dd3bf3ead536183539aeba9d4c
CHANDAN KUMAR SAHNI <142076813+chandan1504@users.noreply.github.com>
2024-11-19 03:08:28 +0000

    datatype done

---
+++ 01-basics/02_datatype.js
@@ -0,0 +1,28 @@
+"use strict" // treat all js code newer version
+
+// alert( 3+3)// we are using node js, not browser
+
+console.log(3+3)
+// code readability should be high
+console. log("Chandan")
+
+
+let name  =  "Chandan"
+let age = 18
+let isLoggedIn = true
+let state = null
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+//undefined =>
+//symbol=> unique
+
+
+// object
+
+console.log(typeof age)
+console.log(typeof null)// object
+console.log(typeof undefined)// undefined 
