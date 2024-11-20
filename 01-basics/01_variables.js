4964ffc58e564f04e83abfe3dacab9daa82a7b6b
CHANDAN KUMAR SAHNI <142076813+chandan1504@users.noreply.github.com>
2024-11-20 17:52:39 +0000

    comparison

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId= "848117"
+let accountEmail ="chandankumarshani123@gmail.com"
+var accountPassword = "8235384950"
+accountCity = "Samastipur"
+let accountState;
+
+// accountId = 2// note allowed
+accountEmail ="anjali123@gamil.com"
+accountPassword= "143-1432"
+accountCity=" patna"
+
+console.log(accountId);
+// Prefer not to use var
+// Because of issues in block scope and function scope
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
--- 01-basics/03_conversionOperation.js
+++ 01-basics/03_conversionOperation.js
@@ -8,8 +8,8 @@ let score  = true
 // console.log(typeof (score));
 
 let valueInNumber = Number = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //" 15" => 15
 // "15abc => NaN"
@@ -19,7 +19,7 @@ console.log(valueInNumber);
 // let isLoggedIn = ""
 let isLoggedIn = "Chandan"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
  //-----------------/*Conversion*?--------------------
 // 1 => true; 0 => false
@@ -29,5 +29,39 @@ console.log(booleanIsLoggedIn)
 
  let someNumber  = 30
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log( typeof stringNumber)
+// console.log(stringNumber)
+// console.log( typeof stringNumber)
+
+//**********************************Operations********************//
+let value = 30
+let negValue = -value 
+// console.log(negValue)
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/4);
+// console.log(10%4);
+let str1 = "Anjali"
+let str2 = " Chandan"
+let str3  = str1 +str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1+ 2);
+// console.log(1+ "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3+4) * 5 % 3 );
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 15 + 4
+
+let gameCounter  = 100
+// gameCounter ++;
+++gameCounter;
+console.log(gameCounter)
